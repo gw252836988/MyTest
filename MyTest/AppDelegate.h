c742a4b8296b59f5bd9ffeef5ea003c8aac11805
新城集团 <xcjt@promote.cache-dns.local>
2016-09-18 11:21:26 +0800

    Initial Commit

---
+++ MyTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MyTest
+//
+//  Created by 新城集团 on 16-9-18.
+//  Copyright (c) 2016年 新城集团. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MyTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MyTest
+//
+//  Created by 新城集团 on 16-9-18.
+//  Copyright (c) 2016年 新城集团. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
